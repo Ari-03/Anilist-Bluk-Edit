0097042d20c2564a1574d07162d4d0810f8029f5
Mac.Ari <d.aritra@outlook.com>
2025-08-25 18:23:27 -0700

    Added Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/contexts/AuthContext'
+import { Analytics } from '@vercel/analytics/next'
 import '@/styles/globals.css'
 import '@/styles/sliders.css'
 
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
             <Component {...pageProps} />
+            <Analytics />
         </AuthProvider>
     )
 } 
